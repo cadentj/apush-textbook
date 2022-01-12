a6d630b79265cec621d37c240ca7ab410a4c8b41
Caden <caden.juang@gmail.com>
2022-01-12 10:21:23 -0600

    Semester 2

--- js/dataset.js
+++ js/dataset.js
@@ -3,21 +3,21 @@ let dataset = [
     "469-478",
     "pdfs/469-478.pdf",
     "https://drive.google.com/file/d/1unONhE0-kesZ7_TpH1kjFsEmI2--eTsr/view?usp=sharing",
-    "assets/chapter-images/44-58.jpg",
+    "assets/chapter-images/469-478.jpg",
     ["plantations", "slaves", "slave trade"]
   ],
   [
     "496-509",
     "pdfs/496-509.pdf",
     "https://drive.google.com/file/d/1-CeKhSxSanI8IKFmH728TZQaZQrzk0-5/view?usp=sharing",
-    "assets/chapter-images/58-65.jpg",
+    "assets/chapter-images/496-509.jpg",
     ["pilgrims", "plymouth"]
   ],
   [
     "509-516",
     "pdfs/509-516.pdf",
     "https://drive.google.com/file/d/1wzLwo3OvQHqbA5-qye9V0Vz-uugn0EDM/view?usp=sharing",
-    "assets/chapter-images/76-79.jpg",
+    "assets/chapter-images/509-516.jpg",
     ["chesapeake"]
   ]
 ]
